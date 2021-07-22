076f49ffdb841191ef3d50d43b46d71e6e157a18
sxweb <phpred@yandex.ru>
2021-07-22 18:22:45 +0300

    use html files in express

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
-})
+});
